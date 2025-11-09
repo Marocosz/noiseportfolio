2214c680b91801b18a245bd6dd1db07ccc650873
Marcos Rodrigues <marcosrodriguesepro@gmail.com>
2025-11-09 05:25:17 -0300

    scroll massa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
 import HomePage from './pages/HomePage';
 
 function App() {
-  return (
-    <HomePage />
-  );
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.3,
+      smooth: true,
+      smoothTouch: true,
+      lerp: 0.07,
+      easing: (t) => t,
+    });
+
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
+
+  return <HomePage />;
 }
 
 export default App;
